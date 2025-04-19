a141289929d0381b452f9bd1cab0b79597182257
Sayed_Mostafa <sayedmostafa01091706102@gmail.com>
2025-04-19 14:33:16 +0200

    onBoardingView: create custom button

---
+++ lib/core/widgets/custom_button.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:fruits_hub/core/utils/app_colors.dart';
+
+class CustomButton extends StatelessWidget {
+  const CustomButton({super.key, required this.onPressed, required this.text});
+  final VoidCallback onPressed;
+  final String text;
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      width: double.infinity,
+      height: 54,
+      child: TextButton(
+        style: TextButton.styleFrom(
+          backgroundColor: AppColors.primaryColor,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(16),
+          ),
+        ),
+        onPressed: onPressed,
+        child: Text(text, style: TextStyle(color: Colors.white)),
+      ),
+    );
+  }
+}
--- lib/features/on_boarding/presentation/views/widgets/on_boarding_view_body.dart
+++ lib/features/on_boarding/presentation/views/widgets/on_boarding_view_body.dart
@@ -1,6 +1,8 @@
 import 'package:dots_indicator/dots_indicator.dart';
 import 'package:flutter/material.dart';
+import 'package:fruits_hub/constants.dart';
 import 'package:fruits_hub/core/utils/app_colors.dart';
+import 'package:fruits_hub/core/widgets/custom_button.dart';
 import 'package:fruits_hub/features/on_boarding/presentation/views/widgets/on_boarding_page_view.dart';
 
 class OnBoardingViewBody extends StatelessWidget {
@@ -18,6 +20,12 @@ class OnBoardingViewBody extends StatelessWidget {
             color: AppColors.primaryColor.withOpacity(0.5),
           ),
         ),
+        SizedBox(height: 29),
+        Padding(
+          padding: EdgeInsets.symmetric(horizontal: kHorizontalPadding),
+          child: CustomButton(onPressed: () {}, text: 'ابدأ الان'),
+        ),
+        SizedBox(height: 43),
       ],
     );
   }
