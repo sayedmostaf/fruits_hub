4f70fae5743d416a8f7af3edda94962c4b1edb60
Sayed_Mostafa <sayedmostafa01091706102@gmail.com>
2025-04-19 11:04:20 +0200

    onBoardingView: setup files

---
+++ lib/features/on_boarding/presentation/views/widgets/on_boarding_page_view.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class OnBoardingPageView extends StatelessWidget {
+  const OnBoardingPageView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return PageView(children: []);
+  }
+}
