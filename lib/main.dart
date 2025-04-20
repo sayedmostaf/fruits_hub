c7fe263ede94770c5e3bebd295d06c9103e0b91b
Sayed_Mostafa <sayedmostafa01091706102@gmail.com>
2025-04-20 14:18:45 +0200

    onBoardingView: create shared preferences singleton

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:fruits_hub/core/helper_functions/on_generate_routes.dart';
+import 'package:fruits_hub/core/services/shared_preferences_singleton.dart';
 import 'package:fruits_hub/features/splash/presentation/views/splash_view.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:fruits_hub/generated/l10n.dart';
 
-void main() {
+void main() async{
+    await Prefs.init();
   runApp(const FruitHub());
 }
 
