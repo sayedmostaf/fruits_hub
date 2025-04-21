8b54ff6847c358071c0813ed1f27c4ce7e38a61f
Sayed_Mostafa <sayedmostafa01091706102@gmail.com>
2025-04-21 12:53:24 +0200

    loginView: create ui p4

--- lib/features/auth/presentation/views/widgets/login_view_body.dart
+++ lib/features/auth/presentation/views/widgets/login_view_body.dart
@@ -4,6 +4,8 @@ import 'package:fruits_hub/core/utils/app_colors.dart';
 import 'package:fruits_hub/core/utils/app_text_styles.dart';
 import 'package:fruits_hub/core/widgets/custom_button.dart';
 import 'package:fruits_hub/core/widgets/custom_text_form_field.dart';
+import 'package:fruits_hub/features/auth/presentation/views/widgets/dont_have_an_accout_widget.dart';
+import 'package:fruits_hub/features/auth/presentation/views/widgets/or_divider.dart';
 
 class LoginViewBody extends StatelessWidget {
   const LoginViewBody({super.key});
@@ -41,31 +43,9 @@ class LoginViewBody extends StatelessWidget {
             const SizedBox(height: 33),
             CustomButton(onPressed: () {}, text: 'تسجيل دخول'),
             const SizedBox(height: 33),
-            Text.rich(
-              TextSpan(
-                children: [
-                  TextSpan(
-                    text: 'لا تمتلك حساب؟',
-                    style: TextStyles.semiBold16.copyWith(
-                      color: Color(0xFF949D9E),
-                    ),
-                  ),
-                  TextSpan(
-                    text: " ",
-                    style: TextStyles.semiBold16.copyWith(
-                      color: Color(0xFF616A6B),
-                    ),
-                  ),
-                  TextSpan(
-                    text: 'قم بإنشاء حساب',
-                    style: TextStyles.semiBold16.copyWith(
-                      color: AppColors.primaryColor,
-                    ),
-                  ),
-                ],
-              ),
-              textAlign: TextAlign.center,
-            ),
+            DontHaveAnAccountWidget(),
+            const SizedBox(height: 33),
+            OrDivider(),
           ],
         ),
       ),
