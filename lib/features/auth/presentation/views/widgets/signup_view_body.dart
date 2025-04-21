c5ce2d2be146f4bf92cab66e96dadc7ef7669f77
Sayed_Mostafa <sayedmostafa01091706102@gmail.com>
2025-04-21 14:43:29 +0200

    signupView: create ui p2

--- lib/features/auth/presentation/views/widgets/signup_view_body.dart
+++ lib/features/auth/presentation/views/widgets/signup_view_body.dart
@@ -1,10 +1,36 @@
 import 'package:flutter/material.dart';
+import 'package:fruits_hub/constants.dart';
+import 'package:fruits_hub/core/widgets/custom_text_form_field.dart';
 
 class SignupViewBody extends StatelessWidget {
   const SignupViewBody({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return SingleChildScrollView(
+      child: Padding(
+        padding: EdgeInsets.symmetric(horizontal: kHorizontalPadding),
+        child: Column(
+          children: [
+            SizedBox(height: 24),
+            CustomTextFormField(
+              hintText: 'الاسم كامل',
+              textInputType: TextInputType.name,
+            ),
+            SizedBox(height: 16),
+            CustomTextFormField(
+              hintText: 'البريد الالكتروني',
+              textInputType: TextInputType.emailAddress,
+            ),
+            SizedBox(height: 16),
+            CustomTextFormField(
+              hintText: 'كلمة المرور',
+              textInputType: TextInputType.visiblePassword,
+              suffixIcon: Icon(Icons.remove_red_eye, color: Color(0xffC9CECF)),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
