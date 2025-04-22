dbb6e330a91df77990bda10e7911e948d03aa1f4
Sayed_Mostafa <sayedmostafa01091706102@gmail.com>
2025-04-22 14:10:15 +0200

    loginView: Add CustomException class to handle Firebase authentication exceptions and update FirebaseAuthService to throw CustomException with appropriate error messages.

--- lib/core/services/firebase_auth_service.dart
+++ lib/core/services/firebase_auth_service.dart
@@ -1,1 +1,25 @@
-class FirebaseAuthService {}
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:fruits_hub/core/errors/esceptions.dart';
+
+class FirebaseAuthService {
+  Future<User> createUserWithEmailAndPassword({
+    required String email,
+    required String password,
+  }) async {
+    try {
+      final credential = await FirebaseAuth.instance
+          .createUserWithEmailAndPassword(email: email, password: password);
+      return credential.user!;
+    } on FirebaseAuthException catch (e) {
+      if (e.code == 'weak-password') {
+        throw CustomException('The password provided is too weak.');
+      } else if (e.code == 'email-already-in-use') {
+        throw CustomException('The account already exists for that email.');
+      } else {
+        throw CustomException('An error occurred. Please try again later.');
+      }
+    } catch (e) {
+      throw CustomException('An error occurred. Please try again later.');
+    }
+  }
+}
