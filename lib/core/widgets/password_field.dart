9d0ef7e7aa1e77ad13efac625fcbc002f17f02ee
Sayed_Mostafa <sayedmostafa01091706102@gmail.com>
2025-04-23 13:14:13 +0200

    signupView: create password field

---
+++ lib/core/widgets/password_field.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:fruits_hub/core/widgets/custom_text_form_field.dart';
+
+class PasswordField extends StatefulWidget {
+  const PasswordField({super.key, this.onSaved});
+  final void Function(String?)? onSaved;
+
+  @override
+  State<PasswordField> createState() => _PasswordFieldState();
+}
+
+class _PasswordFieldState extends State<PasswordField> {
+  bool obscureText = true;
+  @override
+  Widget build(BuildContext context) {
+    return CustomTextFormField(
+      obscureText: obscureText,
+      onSaved: (value) {},
+      suffixIcon: GestureDetector(
+        onTap: () {
+          obscureText = !obscureText;
+          setState(() {});
+        },
+        child:
+            obscureText
+                ? Icon(Icons.remove_red_eye, color: Color(0xffC9CECF))
+                : Icon(Icons.visibility_off, color: Color(0xffC9CECF)),
+      ),
+      hintText: 'كلمة المرور',
+      textInputType: TextInputType.visiblePassword,
+    );
+  }
+}
--- lib/features/auth/presentation/views/widgets/signup_view_body.dart
+++ lib/features/auth/presentation/views/widgets/signup_view_body.dart
@@ -3,6 +3,7 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:fruits_hub/constants.dart';
 import 'package:fruits_hub/core/widgets/custom_button.dart';
 import 'package:fruits_hub/core/widgets/custom_text_form_field.dart';
+import 'package:fruits_hub/core/widgets/password_field.dart';
 import 'package:fruits_hub/features/auth/presentation/cubits/signup_cubits/signup_cubit.dart';
 import 'package:fruits_hub/features/auth/presentation/views/widgets/have_an_account_widget.dart';
 import 'package:fruits_hub/features/auth/presentation/views/widgets/terms_and_conditions_widget.dart';
@@ -45,16 +46,10 @@ class _SignupViewBodyState extends State<SignupViewBody> {
                 textInputType: TextInputType.emailAddress,
               ),
               SizedBox(height: 16),
-              CustomTextFormField(
+              PasswordField(
                 onSaved: (value) {
                   password = value!;
                 },
-                hintText: 'كلمة المرور',
-                textInputType: TextInputType.visiblePassword,
-                suffixIcon: Icon(
-                  Icons.remove_red_eye,
-                  color: Color(0xffC9CECF),
-                ),
               ),
               SizedBox(height: 16),
               TermsAndConditionsWidget(),
