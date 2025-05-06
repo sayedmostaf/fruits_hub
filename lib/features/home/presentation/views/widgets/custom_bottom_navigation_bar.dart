f04b968f5ccaaf94499c84717f6ccad4a4c65680
Sayed_Mostafa <sayedmostafa01091706102@gmail.com>
2025-05-06 19:11:19 +0300

    Add Vuesax icons and implement custom bottom navigation bar
    - Added new Vuesax SVG icons for home, products, shopping cart, and user in both bold and outline styles.
    - Updated asset paths in app_images.dart to include new icons.
    - Implemented a CustomBottomNavigationBar widget to enhance the home view with a bottom navigation bar.
    - Integrated the custom bottom navigation bar into the HomeView.
    - Updated pubspec.yaml to include new asset paths for Vuesax icons.

---
+++ lib/features/home/presentation/views/widgets/custom_bottom_navigation_bar.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:fruits_hub/core/utils/app_images.dart';
+
+class CustomBottomNavigationBar extends StatelessWidget {
+  const CustomBottomNavigationBar({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 375,
+      height: 70,
+      decoration: ShapeDecoration(
+        color: Colors.white,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.only(
+            topLeft: Radius.circular(30),
+            topRight: Radius.circular(30),
+          ),
+        ),
+        shadows: [
+          BoxShadow(
+            color: Color(0x19000000),
+            blurRadius: 25,
+            offset: Offset(0, -2),
+            spreadRadius: 0,
+          ),
+        ],
+      ),
+      child: InActiveItem(image: Assets.imagesVuesaxOutlineHome),
+    );
+  }
+}
+
+class InActiveItem extends StatelessWidget {
+  const InActiveItem({super.key, required this.image});
+  final String image;
+  @override
+  Widget build(BuildContext context) {
+    return SvgPicture.asset(image);
+  }
+}
