f53b0de6e2c2c13b8921965430dd10425e6fe7f4
Sayed_Mostafa <sayedmostafa01091706102@gmail.com>
2025-05-19 15:14:02 +0300

    feat: implement query parameters for getData method in DatabaseService and FireStoreService, and update getBestSellingProducts to utilize new logic

--- lib/core/repos/products_repo/products_repo_impl.dart
+++ lib/core/repos/products_repo/products_repo_impl.dart
@@ -10,8 +10,24 @@ class ProductsRepoImpl extends ProductsRepo {
   final DatabaseService databaseService;
   ProductsRepoImpl(this.databaseService);
   @override
-  Future<Either<Failure, List<ProductEntity>>> getBestSellingProducts() {
-    throw UnimplementedError();
+  Future<Either<Failure, List<ProductEntity>>> getBestSellingProducts() async {
+    try {
+      var data =
+          await databaseService.getData(
+                path: BackendEndpoint.getProducts,
+                query: {
+                  'limit':10,
+                  'orderBy': 'sellingCount',
+                  'descending': true,
+                },
+              )
+              as List<Map<String, dynamic>>;
+      List<ProductEntity> products =
+          data.map((e) => ProductModel.fromJson(e).toEntity()).toList();
+      return right(products);
+    } catch (e) {
+      return left(ServerFailure('Failed to get products'));
+    }
   }
 
   @override
--- lib/core/services/data_service.dart
+++ lib/core/services/data_service.dart
@@ -4,7 +4,11 @@ abstract class DatabaseService {
     required Map<String, dynamic> data,
     String? documentId,
   });
-  Future<dynamic> getData({required String path, String? documentId});
+  Future<dynamic> getData({
+    required String path,
+    String? documentId,
+    Map<String, dynamic>? query,
+  });
   Future<bool> checkIfDataExists({
     required String path,
     required String documentId,
