fe57a8dd8b46bc8b76e87b1ee6604d49232f79dc
Sayed_Mostafa <sayedmostafa01091706102@gmail.com>
2025-05-22 14:00:44 +0300

    feat: update buildAppBar to conditionally show back button and refactor ProductViewBody to use it

--- lib/core/widgets/custom_app_bar.dart
+++ lib/core/widgets/custom_app_bar.dart
@@ -2,12 +2,19 @@ import 'package:flutter/material.dart';
 import 'package:fruits_hub/core/utils/app_text_styles.dart';
 import 'package:fruits_hub/core/widgets/notification_widget.dart';
 
-AppBar buildAppBar(context, {required String title}) {
+AppBar buildAppBar(
+  context, {
+  required String title,
+  bool showBackButton = true,
+}) {
   return AppBar(
     backgroundColor: Colors.white,
-    leading: GestureDetector(
-      onTap: () => Navigator.pop(context),
-      child: Icon(Icons.arrow_back_ios_new),
+    leading: Visibility(
+      visible: showBackButton,
+      child: GestureDetector(
+        onTap: () => Navigator.pop(context),
+        child: Icon(Icons.arrow_back_ios_new),
+      ),
     ),
     actions: const [
       Padding(
--- lib/features/home/presentation/views/widgets/product_view_body.dart
+++ lib/features/home/presentation/views/widgets/product_view_body.dart
@@ -2,9 +2,9 @@ import 'package:flutter/widgets.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:fruits_hub/constants.dart';
 import 'package:fruits_hub/core/cubits/products_cubit/products_cubit.dart';
+import 'package:fruits_hub/core/widgets/custom_app_bar.dart';
 import 'package:fruits_hub/core/widgets/search_text_field.dart';
 import 'package:fruits_hub/features/home/presentation/views/widgets/products_grid_view_bloc_builder.dart';
-import 'package:fruits_hub/features/home/presentation/views/widgets/custom_home_app_bar.dart';
 import 'package:fruits_hub/features/home/presentation/views/widgets/product_view_header.dart';
 
 class ProductViewBody extends StatefulWidget {
@@ -31,7 +31,7 @@ class _ProductViewBodyState extends State<ProductViewBody> {
             child: Column(
               children: [
                 SizedBox(height: kTopPadding),
-                CustomHomeAppBar(),
+                buildAppBar(context, title: 'المنتجات', showBackButton: false),
                 const SizedBox(height: 16),
                 SearchTextField(),
                 const SizedBox(height: 12),
