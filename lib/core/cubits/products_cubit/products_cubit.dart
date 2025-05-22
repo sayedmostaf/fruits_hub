7d5791545a58f1dadda93b3882ed030423c243b3
Sayed_Mostafa <sayedmostafa01091706102@gmail.com>
2025-05-22 14:06:42 +0300

    refactor: reorganize getProducts and getBestSellingProducts methods for improved readability

--- lib/core/cubits/products_cubit/products_cubit.dart
+++ lib/core/cubits/products_cubit/products_cubit.dart
@@ -9,20 +9,20 @@ class ProductsCubit extends Cubit<ProductsState> {
   Future<void> getProducts() async {
     emit(ProductsLoading());
     var result = await productsRepo.getProducts();
-    result.fold(
-      (failure) => emit(ProductsFailure(failure.message)),
-      (products) => emit(ProductsSuccess(products)),
-    );
-  }
-
-  Future<void> getBestSellingProducts() async {
-    emit(ProductsLoading());
-    var result = await productsRepo.getBestSellingProducts();
     result.fold((failure) => emit(ProductsFailure(failure.message)), (
       products,
     ) {
       productsLength = products.length;
       emit(ProductsSuccess(products));
     });
+  }
+
+  Future<void> getBestSellingProducts() async {
+    emit(ProductsLoading());
+    var result = await productsRepo.getBestSellingProducts();
+    result.fold(
+      (failure) => emit(ProductsFailure(failure.message)),
+      (products) => emit(ProductsSuccess(products)),
+    );
   }
 }
