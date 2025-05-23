bc81cb4f0f4a1bdd57a73e430ca0dae81e44436f
Sayed_Mostafa <sayedmostafa01091706102@gmail.com>
2025-05-24 00:28:49 +0300

    feat: replace ActiveStepItem with InActiveStepItem in CheckoutViewBody

--- lib/features/checkout/presentation/views/widgets/checkout_view_body.dart
+++ lib/features/checkout/presentation/views/widgets/checkout_view_body.dart
@@ -1,15 +1,11 @@
 import 'package:flutter/widgets.dart';
-import 'package:fruits_hub/features/checkout/presentation/views/widgets/active_step_item.dart';
+import 'package:fruits_hub/features/checkout/presentation/views/widgets/in_active_step_item.dart';
 
 class CheckoutViewBody extends StatelessWidget {
   const CheckoutViewBody({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        ActiveStepItem(),
-      ],
-    );
+    return Column(children: [InActiveStepItem()]);
   }
 }
